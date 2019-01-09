2acaac5903a62235f30409e244bfebf10581ca36
Edouard DUPIN <yui.heero@gmail.com>
2019-01-09 22:21:39 +0100

    [DEV] start analiser

---
+++ recoAnalyser/appl/debug.cpp
@@ -0,0 +1,13 @@
+/** @file
+ * @author Edouard DUPIN 
+ * @copyright 2019, Edouard DUPIN, all right reserved
+ * @license MPL v2.0 (see license file)
+ */
+
+#include <appl/debug.hpp>
+
+
+int32_t appl::getLogId() {
+	static int32_t g_val = elog::registerInstance("RecoWord");
+	return g_val;
+}
